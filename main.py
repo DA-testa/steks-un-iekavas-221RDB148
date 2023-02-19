94eda7f8574e3717af8882c978d3ddc690fabd1b
221RDB148 <125148699+221RDB148@users.noreply.github.com>
2023-02-19 22:19:45 +0000

    izņēmu no def find_mismatch print statementus un ieliku return tā vietā, (lai printētu main() metode)

--- main.py
+++ main.py
@@ -5,19 +5,12 @@ from collections import namedtuple
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-# b = Bracket('aa',2)                        #MY
-# print(b.char)                              #MY
-
 def are_matching(left, right):                     #parbauda vai iekavas sakrit
     return (left + right) in ["()", "[]", "{}"]    #parbauda vai iekavas sakrit
-
-# if (are_matching("{", "}") == True):       #MY
-#     print("YES")                           #MY
 
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    closing_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i))
@@ -29,43 +22,17 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
         
     if len(opening_brackets_stack) == 0:
-        print("Success")
+        # print("Success")
+        return "Success"
     else:
-        print(opening_brackets_stack[-1].position + 1)
-
-    # print("opening: ", end="")           #PRINT
-    # for i, j in opening_brackets_stack:  #PRINT
-    #     print(i, j, end=" ")             #PRINT
-    # print("\nclosing: ", end="")         #PRINT
-    # for i, j in closing_brackets_stack:  #PRINT
-    #     print(i, j, end=" ")             #PRINT
-    # print()                              #PRINT
-
-    # for i, j in closing_brackets_stack:                              #dead 
-    #     print(j, end=" ")                                            #dead
-    #     # a = len(closing_brackets_stack)-1                          #dead
-    #     print(closing_brackets_stack[len(closing_brackets_stack)-1]) #dead
-
-    # a = closing_brackets_stack[len(closing_brackets_stack)-1].position
-    # print(a)
-
-    # print(opening_brackets_stack.position) #dead
-
-    # for i in range(a-1,-1,-1):
-    #     print(i)
-    #     if i in opening_brackets_stack:
-    #         print(i, "uraaa")
-
-
-
+        # print(opening_brackets_stack[-1].position + 1)
+        return opening_brackets_stack[-1].position + 1
 
 def main():
     text = input()
-    # mismatch = find_mismatch(text)
-    find_mismatch(text)
-
-
-
+    mismatch = find_mismatch(text)
+    print(mismatch)
+    # find_mismatch(text)
 
 if __name__ == "__main__":
     main()
